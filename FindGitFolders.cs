b470f0003db38c497472a132ced535b6ed2737e3
Scott Durret <icodeintx@gmail.com>
2022-04-30 19:59:37 -0500

    initial add

---
+++ FindGitFolders.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindGitFolders
+{
+    internal class FindGitFolders
+    {
+        private List<string> _folders;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public FindGitFolders()
+        {
+            _folders = new List<string>();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="folder"></param>
+        public void Start(string startFolder)
+        {
+            Console.WriteLine($"Starting in folder {startFolder}");
+
+            Recursefolders(startFolder);
+
+            PrintFolders();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="folder"></param>
+        private void AddFolderToList(string folder)
+        {
+            //remove the .git from the name
+            string fixedFolder = folder.Replace(".git", "");
+
+            _folders.Add(fixedFolder);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        private void PrintFolders()
+        {
+            if (_folders.Count > 0)
+            {
+                foreach (string folder in _folders)
+                {
+                    Console.WriteLine(folder);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Git Folders Found!");
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="startFolder"></param>
+        /// <returns></returns>
+        private void Recursefolders(string startFolder)
+        {
+            try
+            {
+                foreach (string folder in Directory.GetDirectories(startFolder))
+                {
+                    if (folder.EndsWith(".git"))
+                    {
+                        AddFolderToList(folder);
+                    }
+
+                    Recursefolders(folder);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FindGitFolders;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string startFolder = "";
+
+        //if no argument passed in then use current folder
+        if (args.Length == 0)
+        {
+            startFolder = Environment.CurrentDirectory;
+        }
+        else
+        {
+            //if argument is passed in then test
+            try
+            {
+                if (Directory.Exists(args[0]))
+                {
+                    startFolder = args[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //create new instance of FindGitFolder class
+        FindGitFolders gf = new();
+        gf.Start(startFolder);
+    }
+}
