d736cf54bde219faa0e389c354797b912cffd029
Scott Durret <icodeintx@gmail.com>
2022-05-09 21:34:38 -0500

    commented code

--- FindGitFolders.cs
+++ FindGitFolders.cs
@@ -11,43 +11,56 @@ namespace FindGitFolders
         private List<string> _folders;
 
         /// <summary>
-        ///
+        /// Simple class to travers folders and display folder that 
+        /// contain a .git folder
         /// </summary>
         public FindGitFolders()
         {
+            //create module level collection to store results.
             _folders = new List<string>();
         }
 
         /// <summary>
-        ///
+        ///  Entry point into this class
         /// </summary>
         /// <param name="folder"></param>
         public void Start(string startFolder)
         {
+            //display simple message
             Console.WriteLine($"Starting in folder {startFolder}");
 
+            //recurse from starting folder
             Recursefolders(startFolder);
 
+            //print folders in our collection
             PrintFolders();
         }
 
+
+
         /// <summary>
-        ///
+        /// Add the folder to our collection
         /// </summary>
         /// <param name="folder"></param>
         private void AddFolderToList(string folder)
         {
+            //Since the folder name is like \src\myapp\.git
             //remove the .git from the name
             string fixedFolder = folder.Replace(".git", "");
 
+            //add the corrected name to the collection
             _folders.Add(fixedFolder);
         }
 
+
+
         /// <summary>
-        ///
+        ///  Display the folders in our collection to the screen
         /// </summary>
         private void PrintFolders()
         {
+
+            //first ensure we have at least one folder to display
             if (_folders.Count > 0)
             {
                 foreach (string folder in _folders)
@@ -57,12 +70,13 @@ namespace FindGitFolders
             }
             else
             {
+                //if nothing in the collection the display simple message
                 Console.WriteLine("No Git Folders Found!");
             }
         }
 
         /// <summary>
-        ///
+        /// Recursivly travers all the folders from the starting folder
         /// </summary>
         /// <param name="startFolder"></param>
         /// <returns></returns>
@@ -70,19 +84,22 @@ namespace FindGitFolders
         {
             try
             {
+                //Loop every folder from the starting folder
                 foreach (string folder in Directory.GetDirectories(startFolder))
                 {
+                    //if the folder name ends with .git then add this folder to our collection
                     if (folder.EndsWith(".git"))
                     {
                         AddFolderToList(folder);
                     }
 
+                    //now call ourself recursivly to travers every branch.
                     Recursefolders(folder);
                 }
             }
             catch (Exception ex)
             {
-                //Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
             }
         }
     }
